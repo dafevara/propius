2c1d9fdb5848de91b03078c4881c669701a96438
Daniel Valencia <dafevara@gmail.com>
2021-06-09 18:40:42 -0500

    fixed typo
    Signed-off-by: Daniel Valencia <dafevara@gmail.com>

--- propius/basic.py
+++ propius/basic.py
@@ -1,15 +1,28 @@
-from utils import stream_csv
-from model import SimilarityModel
+import utils
+import model
 
 import pandas as pd
 
-dictionary = pd.read_csv('/tmp/dictionary.csv')
-item_occurrences = stream_csv('/tmp/ordered_person_titles.csv')
-
-model = SimilarityModel(
-    item_occurrences,
-    2739984
+dictionary = pd.read_csv(
+    '/Users/dafevara/PROJECTS/propius/examples/dictionary.csv'
+)
+item_occurrences = utils.stream_csv(
+    '/Users/dafevara/PROJECTS/propius/examples/imp_ordered_person_titles.csv'
 )
 
-model.build()
-df = model.as_dataframe()
+sim_model = model.SimilarityModel(
+    dictionary,
+    item_occurrences,
+    2399055
+)
+
+sim_model.build()
+df = sim_model.as_dataframe()
+
+storer = model.ModelStorer(
+    sim_model
+)
+# breakpoint()
+storer.prepare()
+storer.populate_correlated_items()
+storer.populate_similar_items()
