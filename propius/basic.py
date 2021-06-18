6518b07cd6f054cdc263a1f0d661172e6e0c2736
Daniel Valencia <dafevara@gmail.com>
2021-06-18 12:36:00 -0500

    removed wrong paths for input files
    Signed-off-by: Daniel Valencia <dafevara@gmail.com>

--- propius/basic.py
+++ propius/basic.py
@@ -4,16 +4,16 @@ import model
 import pandas as pd
 
 dictionary = pd.read_csv(
-    '/Users/dafevara/PROJECTS/propius/examples/dictionary.csv'
+    '/.../dictionary.csv'
 )
 item_occurrences = utils.stream_csv(
-    '/Users/dafevara/PROJECTS/propius/examples/imp_ordered_person_titles.csv'
+    '/.../item_co_occurrences.csv'
 )
 
 sim_model = model.SimilarityModel(
     dictionary,
     item_occurrences,
-    2399055  # num of occurrences must be previously known
+    1_234_567  # num of occurrences must be previously known
 )
 
 sim_model.build()
